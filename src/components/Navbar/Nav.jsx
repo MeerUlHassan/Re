96472aab7612a758e0f4a70baf8a07441030aed9
MeerUlHassan <github4421@gmail.com>
2023-09-29 10:02:29 -0700

    Floating NavBar comp

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,12 +1,52 @@
-import React from 'react'
-import './nav.css'
+import React, { useState } from "react";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { LuMessagesSquare } from "react-icons/lu";
+import "./nav.css";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#service"
+        onClick={() => setActiveNav("#service")}
+        className={activeNav === "#service" ? "active" : ""}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <LuMessagesSquare />
+      </a>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
