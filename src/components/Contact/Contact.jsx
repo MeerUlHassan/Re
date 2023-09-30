2da05c9ebddb5fba5f7ec6459ab686337993092d
MeerUlHassan <github4421@gmail.com>
2023-09-30 10:42:23 -0700

    contact & exp Section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,59 @@
 import React from 'react'
 import './contact.css'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+import {MdOutlineMarkEmailRead} from 'react-icons/md'
+import {PiMessengerLogoBold} from 'react-icons/pi'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_8ehmqfq', 'template_81ah7w3', form.current, 'a0mUGABVxc1FbCEaT')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
   return (
-    <div>
-      
-    </div>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMarkEmailRead />
+            <h4>Email</h4>
+            <h5>Emial Me</h5>
+            <a href="mailto:meer.hassan1227@gmail.com" target='_blank'>Send an Email</a>
+          </article>
+          <article className="contact__option">
+            <PiMessengerLogoBold />
+            <h4>Messenger</h4>
+            <h5>Let's Chat on Messesnger</h5>
+            <a href="https://m.me/?" target='_blank'>Send a Message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp />
+            <h4>WhatsApp</h4>
+            <h5>Click below to Chat!</h5>
+            <a href="https://wa.me/+923097280432" target='_blank'>WhatsApp</a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name Here' required/>
+          <input type="email" name='email' placeholder='Enter Email Here' required />
+          <textarea name="message" rows="7" placeholder='Type Message Here!'></textarea>
+          <button onClick={() => alert("Email has been Sent")} type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,12 +1,83 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
 
-export default Experience
+      <div className="container exp__container">
+        <div className="exp__frontend">
+          <h3>FrontEnd Development</h3>
+          <div className="exp__content">
+            <article className="exp__details">
+              <BsPatchCheckFill />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Expericenced</small>
+              </div>
+            </article>
+            <article className="exp__details">
+              <BsPatchCheckFill />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="exp__details">
+              <BsPatchCheckFill />
+              <div>
+                <h4>BOOTSTRAP</h4>
+                <small className="text-light">Expericenced</small>
+              </div>
+            </article>
+            <article className="exp__details">
+              <BsPatchCheckFill />
+              <div>
+                <h4>JAVASCRIPT</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="exp__details">
+              <BsPatchCheckFill />
+              <div>
+                <h4>REACT</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="exp__backend">
+          <h3>BackEnd Development</h3>
+          <div className="exp__content">
+            <article className="exp__details">
+              <BsPatchCheckFill />
+              <div>
+                <h4>Node</h4>
+                <small className="text-light">Basics</small>
+              </div>
+            </article>
+            <article className="exp__details">
+              <BsPatchCheckFill />
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="exp__details">
+              <BsPatchCheckFill />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
