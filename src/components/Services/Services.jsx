f91e7b8068479539b80ed3418f514c25a4818fc6
MeerUlHassan <github4421@gmail.com>
2023-09-30 10:40:38 -0700

    Service Section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,12 +1,107 @@
-import React from 'react'
-import './services.css'
+import React from "react";
+import "./services.css";
+import { BiBadgeCheck } from "react-icons/bi";
 
 const Services = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section id="service">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
 
-export default Services
+      <div className="container service__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiBadgeCheck className="list__icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
