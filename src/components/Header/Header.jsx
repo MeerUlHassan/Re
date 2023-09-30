e8aa508690dfb7024afe10c922eb9d41ecab7507
MeerUlHassan <github4421@gmail.com>
2023-09-30 10:41:15 -0700

    entered full name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'am</h5>
-        <h1>Meer Hassan </h1>
+        <h1>Meer-Ul-Hassan </h1>
         <h5 className="text-light"> FullStack Developer</h5>
         <CTA />
         <HeaderSocials />
